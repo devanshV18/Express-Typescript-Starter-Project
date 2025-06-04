6ee828e73ce4add7287f00b8fe37fee3e9770122
Devansh Verma <143462246+devanshV18@users.noreply.github.com>
2025-06-04 21:49:49 +0530

    checkpoint 6 -> default error handling by express.

--- src/controllers/pingHandler.ts
+++ src/controllers/pingHandler.ts
@@ -1,8 +1,24 @@
-import { Request, Response } from "express";
+import { NextFunction, Request, Response } from "express";
+import fs from "fs"
 
-export const pingHandler = (req: Request, res:Response) => {
-    // console.log(req.body)
-    // console.log(req.query)
-    // console.log(req.params)
-    res.send("Pong!")
+// export const pingHandler = (req: Request, res:Response, next: NextFunction) => {
+//     // console.log(req.body)
+//     // console.log(req.query)
+//     // console.log(req.params)
+//     res.status(200).json({
+//         success: true,
+//         message: "Pong!"
+//     })
+// }
+
+//demonstrating error handling for an async middleware using next() by calling next(err) that by passes all the non-error handling middleware and invokes the first error handling middlewware -> in this case the default one -> The default error handler successfully handles the error and preserves the server from crashing.
+
+export const pingHandler = async (req: Request, res:Response, next: NextFunction) => {
+    await fs.readFile("sample", (err, data) => {
+        if(err){
+            console.log("Error in async flow", err )
+            next(err) //this line ensures the error handling
+        }
+    })
 }
+
