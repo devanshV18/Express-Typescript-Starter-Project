f696a04e607fee38a912590547155ef78ca3ebf2
Devansh Verma <143462246+devanshV18@users.noreply.github.com>
2025-06-07 17:25:50 +0530

    integrated sequelize and sequelize CLI

--- src/config/index.ts
+++ src/config/index.ts
@@ -4,6 +4,13 @@ type ServerConfig = {
     PORT: number
 }
 
+type DBConfig = {
+    DB_HOST: string,
+    DB_USER: string,
+    DB_PASSWORD: string,
+    DB_NAME: string
+}
+
 function loadEnv(){
     dotenv.config() //config function in dotnet package enables us to load all env vars from .env file so that we cann access them using process global object
     console.log("Env vars loaded")
@@ -13,4 +20,11 @@ loadEnv() //calling the function here itself to load all env vars before executi
 
 export const serverConfig: ServerConfig = {
     PORT: Number(process.env.PORT)
+}
+
+export const dbConfig: DBConfig = {
+    DB_HOST: process.env.DB_HOST || 'localhost',
+    DB_USER: process.env.DB_USER || 'root',
+    DB_PASSWORD: process.env.DB_PASSWORD || 'root',
+    DB_NAME: process.env.DB_NAME || 'test_db'
 }
